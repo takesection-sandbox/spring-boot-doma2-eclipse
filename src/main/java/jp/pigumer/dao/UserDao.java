31808ca933f78b6b0ca8e0d7f4c72ce057eec14c
s_edward <shige@pigumer.gr.jp>
2015-11-03 20:02:01 +0900

    Remove @AnnotateWith

--- src/main/java/jp/pigumer/dao/UserDao.java
+++ src/main/java/jp/pigumer/dao/UserDao.java
@@ -1,19 +1,10 @@
 package jp.pigumer.dao;
 
-import javax.inject.Inject;
-import org.seasar.doma.AnnotateWith;
-import org.seasar.doma.Annotation;
-import static org.seasar.doma.AnnotationTarget.CLASS;
-import static org.seasar.doma.AnnotationTarget.CONSTRUCTOR;
 import org.seasar.doma.Dao;
 import org.seasar.doma.Select;
 import org.seasar.doma.boot.ConfigAutowireable;
-import org.springframework.stereotype.Component;
 
 @ConfigAutowireable
-@AnnotateWith(annotations = {
-    @Annotation(target = CLASS, type=Component.class),
-    @Annotation(target = CONSTRUCTOR, type=Inject.class)})
 @Dao
 public interface UserDao {
    
--- src/test/java/jp/pigumer/dao/TestQuery.java
+++ src/test/java/jp/pigumer/dao/TestQuery.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jp.pigumer.dao;
 
 import java.util.function.Function;
 import java.util.stream.Stream;
 
-/**
- *
- * @author take
- */
 public class TestQuery {
  
     public Long testQuery(Function<Stream<String>, Long> function)
